2f654c4689a8bc7130268069f6641a8a3b4e6dcf
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-14 09:29:31 -0300

    Classes - The Basics

---
+++ Section_07_Classes_The_Basics/01_Exercise.py
@@ -0,0 +1,12 @@
+"""
+Exercise No. 01
+
+Create the simplest class in Python and name it Vehicle.
+
+Tip:
+    Use the pass statement.
+"""
+
+
+class Vehicle:
+    pass
---
+++ Section_07_Classes_The_Basics/02_Exercise.py
@@ -0,0 +1,16 @@
+"""
+Exercise No. 02
+
+Create the simplest Python the class named Phone and display its type to the console.
+
+Expected result:
+
+    <class 'type'>
+"""
+
+
+class Phone:
+    pass
+
+
+print(type(Phone))
