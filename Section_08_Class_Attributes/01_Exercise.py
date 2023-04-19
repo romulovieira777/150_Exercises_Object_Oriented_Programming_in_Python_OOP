f51f177ddf99f7f3b67c11157787bd8a4832afa6
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-19 15:30:07 -0300

    Class Attributes

---
+++ Section_08_Class_Attributes/01_Exercise.py
@@ -0,0 +1,19 @@
+"""
+Exercise No. 01
+
+Implement a class named Phone. In the Phone class, define a class attribute named brand and set its value to 'Apple'.
+
+Then, using dot notation and print() function, display the value of the brand attribute of the Phone class to the
+console.
+
+Expected result:
+
+    Apple
+"""
+
+
+class Phone:
+    brand = 'Apple'
+
+
+print(Phone.brand)
