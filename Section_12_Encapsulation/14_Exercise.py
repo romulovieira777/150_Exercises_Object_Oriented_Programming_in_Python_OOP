5f82298772ceb8577589bebe82c295b755966282
Romulo Vieira <romulo.vieira777@gmail.com>
2023-05-10 09:26:47 -0300

    Encapsulation

--- Section_12_Encapsulation/14_Exercise.py
+++ Section_12_Encapsulation/14_Exercise.py
@@ -23,6 +23,7 @@ Expected result:
 
     The value of age must be of type int.
 """
+# Solution I
 
 
 class Pet:
@@ -57,3 +58,43 @@ except (TypeError, ValueError) as e:
     print(e)
 else:
     print(pet.__dict__)
+
+
+# Solution II
+class Pet:
+
+    def __init__(self, name, age):
+        self._name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise TypeError(
+                'The value of age must be of type int.'
+            )
+        if not value > 0:
+            raise ValueError(
+                'The value of age must be a positive integer.'
+            )
+        self._age = value
+
+
+try:
+    pet = Pet('Max', 'seven')
+except TypeError as error:
+    print(error)
+except ValueError as error:
+    print(error)
