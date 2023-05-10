8cb86005c20953083233dd30069bb640637d35fc
Romulo Vieira <romulo.vieira777@gmail.com>
2023-05-10 09:23:44 -0300

    Encapsulation

---
+++ Section_12_Encapsulation/13_Exercise.py
@@ -0,0 +1,55 @@
+"""
+Exercise No. 13
+
+Implement a class named Pet that has two protected instance attributes: name and age, respectively. Then, using the
+@property decorator, create properties: name and age, respectively (properties to read and modify, without validation).
+
+Create an instance of the Pet class with the name pet and attributes:
+
+    - name = 'Max'
+    - age = 5
+
+Print the __dict__ attribute of the pet instance to the console. Then modify the attributes using the dot notation:
+
+    - name to the value 'Tom'
+    - age to the value 8
+
+Again, print the __dict__ attribute of the pet instance to the console again.
+
+Expected result:
+
+    {'_name': 'Max', '_age': 5}
+    {'_name': 'Tom', '_age': 8}
+"""
+
+
+class Pet:
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value: int):
+        self._age = value
+
+
+pet = Pet('Max', 5)
+
+print(pet.__dict__)
+
+pet.name = 'Tom'
+pet.age = 8
+
+print(pet.__dict__)
---
+++ Section_12_Encapsulation/14_Exercise.py
@@ -0,0 +1,59 @@
+"""
+Exercise No. 14
+
+A class called Pet is implemented that has two properties: name and age (see below). Add validation to the age property
+at the stage of object creation and attribute modification:
+
+    - the value of the age attribute must be an int type, otherwise raise a TypeError with the following message:
+
+        'The value of age must be of type int.'
+
+    - the value of the age attribute must be positive, otherwise raise a ValueError with the following message:
+
+        'The value of age must be a positive integer.'
+
+Then try to create an instance of the Pet class named pet and set the following values:
+
+    - 'Max'
+    - 'seven'
+
+If there is an error, print an error message to the console. use a try ... except clause in your solution.
+
+Expected result:
+
+    The value of age must be of type int.
+"""
+
+
+class Pet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @property
+    def age(self) -> int:
+        return self._age
+
+    @age.setter
+    def age(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError('The value of age must be of type int.')
+        if value < 0:
+            raise ValueError('The value of age must be a positive integer.')
+        self._age = value
+
+
+try:
+    pet = Pet('Max', 'seven')
+except (TypeError, ValueError) as e:
+    print(e)
+else:
+    print(pet.__dict__)
