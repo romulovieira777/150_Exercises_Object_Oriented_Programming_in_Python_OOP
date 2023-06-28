431bf0b3761ea447c5242f591740fc5ddd2687d1
Romulo Vieira <romulo.vieira777@gmail.com>
2023-06-28 09:30:22 -0300

    Inheritance

---
+++ Section_17_Inheritance/10_Exercise.py
@@ -0,0 +1,24 @@
+"""
+Exercise No. 10
+
+Implement simple classes with the following structure:
+
+    - Container
+        - TemperatureControlledContainer
+            - RefrigeratedContainer
+
+The TemperatureControlledContainer class inherits from the Container class and the RefrigeratedContainer class inherits
+from TemperatureControlledContainer.
+"""
+
+
+class Container:
+    pass
+
+
+class TemperatureControlledContainer(Container):
+    pass
+
+
+class RefrigeratedContainer(TemperatureControlledContainer):
+    pass
